bb8dd3790a2bc1ec3f052c6c301c922e690281db
kdomino <kdomino@iitis.pl>
2017-09-12 15:59:48 +0200

    new type def in operations.jl

--- src/operations.jl
+++ src/operations.jl
@@ -32,7 +32,7 @@ julia> vecnorm(st, k=1)
 1.339089
 ```
 """
-function vecnorm{T <: AbstractFloat, N}(bt::SymmetricTensor{T, N}, k::Union{Float64, Int}=2)
+function vecnorm(bt::SymmetricTensor{T, N}, k::Union{Float64, Int}=2) where T<:AbstractFloat where N
   k != 0 || throw(AssertionError("0' th norm not supported"))
   ret = 0.
   for i in indices(N, bt.bln)
@@ -42,14 +42,13 @@ function vecnorm{T <: AbstractFloat, N}(bt::SymmetricTensor{T, N}, k::Union{Floa
 end
 
 """
-  cnorms{T <: AbstractFloat}(c::Vector{SymmetricTensor{T}}, norm::Bool, k::Union{Float64, Int})
+  cnorms(c::Vector{SymmetricTensor{T}}, norm::Bool, k::Union{Float64, Int})
 
 Returns vector of Floats of k norms of cumulants of order 1, ..., m. If norm = true
 .....
 
 """
-function cnorms{T <: AbstractFloat}(c::Vector{SymmetricTensor{T}}, norm::Bool = true,
-                                                                      k::Union{Float64, Int}=2)
+function cnorms(c::Vector{SymmetricTensor{T}}, norm::Bool = true, k::Union{Float64, Int}=2) where T <: AbstractFloat
   if norm
     mcov = [vecnorm(cum, k) for cum in c[1:2]]
     return [mcov..., [vecnorm(c[i], k)/(mcov[2])^(i/2) for i in 3:length(c)]...]
@@ -59,24 +58,24 @@ function cnorms{T <: AbstractFloat}(c::Vector{SymmetricTensor{T}}, norm::Bool = 
 end
 
 """
-  normperelementT <: AbstractFloat}(c::Vector{SymmetricTensor{T}}, norm::Bool, k::Union{Float64, Int})
+  normperelement(c::Vector{SymmetricTensor{T}}, norm::Bool, k::Union{Float64, Int})
 
 Returns vector of Floats of k norms of cumulants of order 1, ..., m, each value is divided by a number of elements in a given cumulant's tensor)
 
 """
 
-normperelement{T <: AbstractFloat}(c::Vector{SymmetricTensor{T}}, norm::Bool = true, k::Union{Float64, Int}=2) =
+normperelement(c::Vector{SymmetricTensor{T}}, norm::Bool = true, k::Union{Float64, Int}=2) where T <: AbstractFloat =
  [vecnorm(c[i], k)/(c[i].dats)^i for i in 1:length(c)]
 
 """
-  cumnorms{T <: AbstractFloat}(X::Matrix{T}, m::Int = 4, norm::Bool = true, k::Union{Float64, Int}=2)
+  cumnorms(X::Matrix{T}, m::Int = 4, norm::Bool = true, k::Union{Float64, Int}=2)
 
 Given multivariate data X computes a vector of cumulants of order 1,...,m cash them and data
 in /tmp/cumdata.jld and returns a vector of Floats of k norm of those cumulants.
 If norm = true each value is normalised (divided by a number of elements in a given cumulant's tensor)
 """
 
-function cumnorms{T <: AbstractFloat}(X::Matrix{T}, m::Int = 4, norm::Bool = true, k::Union{Float64, Int}=2, b::Int = 3)
+function cumnorms(X::Matrix{T}, m::Int = 4, norm::Bool = true, k::Union{Float64, Int}=2, b::Int = 3) where T <: AbstractFloat
   c = cumulants(X, m, b)
   cpath = "/tmp/cumdata.jld"
   save(cpath, "c", c, "x", X)
@@ -84,7 +83,7 @@ function cumnorms{T <: AbstractFloat}(X::Matrix{T}, m::Int = 4, norm::Bool = tru
 end
 
 """
-  cumupdatnorms{T <: AbstractFloat}(X::Matrix{T}, norm::Bool = true, k::Union{Float64, Int}=2)
+  cumupdatnorms(X::Matrix{T}, norm::Bool = true, k::Union{Float64, Int}=2)
 
 Given multivariate data X and vector of cumulants of order 1,...,m stored in /tmp/cumdata.jld
 updates data by Xup and cumulants, store them in "/tmp/cumdata.jld"
@@ -92,7 +91,7 @@ and returns a vector of Floats of k norm of updated cumulants.
 If norm = true each value is normalised (divided by a number of elements in a given cumulant's tensor)
 """
 
-function cumupdatnorms{T <: AbstractFloat}(Xup::Matrix{T}, norm::Bool = true, k::Union{Float64, Int}=2)
+function cumupdatnorms(Xup::Matrix{T}, norm::Bool = true, k::Union{Float64, Int}=2) where T <: AbstractFloat
   cpath = "/tmp/cumdata.jld"
   isfile(cpath) || throw(AssertionError("no cumulants cashed please run cumnorms first"))
   c = load(cpath, "c")
