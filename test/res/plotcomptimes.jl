e02e340a6d159901cda4d31403e7fa22e6ade954
kdomino <kdomino@iitis.pl>
2017-12-14 10:20:58 +0100

    performance test of frequency analysis

--- test/res/plotcomptimes.jl
+++ test/res/plotcomptimes.jl
@@ -4,20 +4,14 @@ using PyCall
 @pyimport matplotlib as mpl
 mpl.rc("text", usetex=true)
 mpl.use("Agg")
+@pyimport matplotlib.ticker as mti
 using PyPlot
 using JLD
 using ArgParse
 
 o = 1
-function singleplot(filename::String, name::String, compare::String = "")
-    d = load(filename*".jld")
-  if compare == ""
-    comptimes = d[name]
-    ylab = "computional time [s]"
-  else
-    comptimes = (d[name]./d[compare])
-    ylab = "speedup"
-  end
+function singleplot(filename::String)
+  d = load(filename*".jld")
   x = d["x"]
   t = d["t"]
   n = d["n"]
@@ -26,19 +20,16 @@ function singleplot(filename::String, name::String, compare::String = "")
   fig, ax = subplots(figsize = (2.5, 2.))
   col = ["red", "blue", "green", "gray", "brown", "orange"]
   marker = [":s", ":o", ":v", ":<", ":>", ":d"]
-  for i in 1:size(comptimes, 2)
-    if contains(filename, "nblocks")
-      str = replace(@sprintf("%.0e", t[i]), "0", "")
+  for i in 1:size(d["cumulants"], 2)
+    if contains(filename, "nblocks") | contains(filename, "nprocs") | contains(filename, "maxf")
+      str = replace(@sprintf("%.1e", t[i]), "0", "")
       tt = "\$"*replace(str, "e+", "\\times 10^{")*"}\$"
-      ax[:plot](d[x], comptimes[:,i], marker[i], label= "\$ t_{up} \$ = $tt", color = col[i], markersize=2.5, linewidth = 1)
-    elseif contains(filename, "nprocs")
-      str = replace(@sprintf("%.0e", t[i]), "0", "")
-      tt = "\$"*replace(str, "e+", "\\times 10^{")*"}\$"
-      ax[:plot](d[x], comptimes[:,i], marker[i], label= "\$ t_{up} \$ = $tt", color = col[i], markersize=2.5, linewidth = 1)
+      ax[:plot](d[x], d["cumulants"][:,i], marker[i], label= "\$ t_{up} \$ = $tt", color = col[i], markersize=2.5, linewidth = 1)
     else
       tt =  n[i]
-      ax[:plot](d[x][o:end], comptimes[o:end,i], marker[i], label= "n = $tt", color = col[i], markersize=2.5, linewidth = 1)
-      i == size(comptimes, 2)? ax[:plot](d[x][o:end], d["tm"][o:end], ":<", label= "theoretical", color = "black", markersize=2.5, linewidth = 1):()
+      comptimes = d["cumulants"][:,i]./d["cumulants updat"][:,i]
+      ax[:plot](d[x], comptimes, marker[i], label= "n = $tt", color = col[i], markersize=2.5, linewidth = 1)
+      i == size(d["cumulants"], 2)? ax[:plot](d[x], d["tm"], ":<", label= "theoretical", color = "black", markersize=2.5, linewidth = 1):()
     end
   end
   subplots_adjust(bottom = 0.16, top=0.92, left = 0.12, right = 0.92)
@@ -47,38 +38,24 @@ function singleplot(filename::String, name::String, compare::String = "")
   ax[:xaxis][:set_major_formatter](fx)
   if contains(filename, "nblocks") | contains(filename, "nprocs")
     subplots_adjust(left = 0.15)
-    PyPlot.ylabel(ylab, labelpad = 0.6)
+    PyPlot.ylabel("computional time [s]", labelpad = 0.6)
     PyPlot.xlabel(x, labelpad = 0.6)
+    ax[:xaxis][:set_major_locator](mti.MaxNLocator(integer=true))
     ax[:legend](fontsize = 5, loc = 9, ncol = 1)
+  elseif contains(filename, "maxf")
+    subplots_adjust(left = 0.18)
+    PyPlot.ylabel("frequency [Hz]", labelpad = 0.)
+    PyPlot.xlabel(x, labelpad = 0.6)
+    ax[:legend](fontsize = 5, loc = 1, ncol = 1)
   else
-    PyPlot.ylabel(ylab, labelpad = -1.0)
+    PyPlot.ylabel("speedup", labelpad = -1.0)
     PyPlot.xlabel("\$ t_{up} \$", labelpad = 0.6)
     ax[:legend](fontsize = 6, loc = 1, ncol = 1)
   end
-  if maximum(comptimes) > 10
-    f = matplotlib[:ticker][:ScalarFormatter]()
-    f[:set_powerlimits]((-3, 2))
-  else
-    f = matplotlib[:ticker][:FormatStrFormatter]("%.1f")
-  end
+  f = matplotlib[:ticker][:ScalarFormatter]()
+  f[:set_powerlimits]((-3, 2))
   ax[:yaxis][:set_major_formatter](f)
-  fig[:savefig](name*filename*".eps")
-end
-
-
-"""
-  pltspeedup(comptimes::Array{Float}, m::Int, n::Vector{Int}, T::Vector{Int}, label::String)
-
-Returns a figure in .eps format of the computional speedup of cumulants function
-
-"""
-
-function pltspeedup(filename::String)
-  d = load(filename)
-  filename = replace(filename, ".jld", "")
-  for f in d["functions"]
-    singleplot(filename::String, f...)
-  end
+  fig[:savefig]("cumulants"*filename*".eps")
 end
 
 
@@ -90,7 +67,8 @@ function main(args)
     arg_type = String
   end
   parsed_args = parse_args(s)
-  pltspeedup(parsed_args["file"])
+  filename = parsed_args["file"]
+  singleplot(replace(filename, ".jld", ""))
 end
 
 main(ARGS)
